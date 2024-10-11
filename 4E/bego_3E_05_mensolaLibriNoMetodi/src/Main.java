a2f3ea9b39975543710a215cb20b543a7aa4669b
obbego <francesco.bego@iisviolamarchesini.edu.it>
2024-10-11 17:35:27 +0200

    add some try catch + duplicates check

--- 4E/bego_3E_05_mensolaLibriNoMetodi/src/Main.java
+++ 4E/bego_3E_05_mensolaLibriNoMetodi/src/Main.java
@@ -1,9 +1,42 @@
+import static frontScreen.FrontEnd.*;
 import static utility.Tools.*;
-import frontScreen.*;
 import mensola.*;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
-        private final  int MAX_LIBRI = 20;
+        Scanner keyboard = new Scanner(System.in);
+        final  int MAX_LIBRI = 20;
+        final String[] elenco = {
+                "Libreria",
+                "Inserisci Libro",
+                "Visualizza libri inseriti",
+                "Esci"
+        };
+        Libro[] scaffale = new Libro[MAX_LIBRI];
+        int nLibri = 0;
+        boolean fine = true;
+
+        do{
+            switch (Menu(elenco, keyboard))
+            {
+                case 1:
+                    if (nLibri < MAX_LIBRI)
+                    {
+                    scaffale[nLibri] = LeggiLibro(keyboard, nLibri, scaffale);
+                    nLibri++;
+                    }
+                    else {
+                        System.out.println("Lo scaffale è pieno");
+                    }
+                    break;
+                case 2:
+                    visualizza(scaffale, nLibri);
+                    break;
+                case 3:
+                    fine = false;
+                    break;
+            }
+        }while(fine);
     }
 }
--- 4E/bego_3E_05_mensolaLibriNoMetodi/src/frontScreen/FrontEnd.java
+++ 4E/bego_3E_05_mensolaLibriNoMetodi/src/frontScreen/FrontEnd.java
@@ -6,17 +6,33 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class FrontEnd {
-    public static Libro leggiLibro(Scanner keyboard){
+    public static Libro LeggiLibro(Scanner keyboard, int nLibri, Libro[] scaffale){
         Libro output = new Libro();
+        boolean trovato, dataAcc;
         //Inserimento autore
-        System.out.println("Inserisci l'autore: ");
-        output.autore = keyboard.nextLine();
-        //Inserimento titolo
-        System.out.println("Inserisci il titolo: ");
-        output.titolo = keyboard.nextLine();
+        do {
+            System.out.println("Inserisci l'autore: ");
+            output.autore = keyboard.nextLine();
+            //Inserimento titolo
+            System.out.println("Inserisci il titolo: ");
+            output.titolo = keyboard.nextLine();
+            trovato = verificaDuplicato(output.autore, output.titolo, nLibri, scaffale);
+            if (trovato)
+                System.out.println("Esiste già un libro con lo stesso titolo e autore");
+
+        }while(trovato);
         //Inserire numero di pagine
-        System.out.println("Inserisci il numero di pagine: ");
-        output.numeroPagine = Integer.parseInt(keyboard.nextLine());
+        do {
+            System.out.println("Inserisci il numero di pagine: ");
+            try {
+                output.numeroPagine = Integer.parseInt(keyboard.nextLine());
+            }
+            catch (Exception e)
+            {
+                System.out.println("Inserisci un numero.");
+                output.numeroPagine = -1;
+            }
+        }while (output.numeroPagine <= 0);
         //Inserimento tipologia
         Genere[] generi = Genere.values();
         String[] menuData = new String[generi.length + 1];
@@ -26,9 +42,36 @@ public class FrontEnd {
         int genereIndex = Menu(menuData, keyboard) - 1;
         output.tipologia = generi[genereIndex];
         //Inserimento data
-        System.out.println("Inserire data: ");
-        String dataInput = keyboard.nextLine();
+        do {
+            dataAcc = true;
+            System.out.println("Inserire data nel formato dd/MM/yyyy: ");
+            String dataInput = keyboard.nextLine();
+            try {
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                output.dataPubblicazione = LocalDate.parse(dataInput, formatter);
+            } catch (Exception e) {
+                System.out.println("La data non rispetta la formattazione corretta");
+                dataAcc = false;
+            }
+        }while (!dataAcc);
+
+        return output;
+    }
+
+    // Metodo che verifica se esiste già un libro con lo stesso autore e titolo
+    private static boolean verificaDuplicato(String autore, String titolo, int nLibri, Libro[] scaffale) {
+        for (int i = 0; i < nLibri; i++) {
+            if (scaffale[i].autore.equals(autore) && scaffale[i].titolo.equals(titolo)) {
+                return true;  // Trovato duplicato (autore e titolo già presenti)
+            }
+        }
+        return false;  // Nessun duplicato trovato
+    }
 
 
+    public static void visualizza(Libro[] scaffale, int nLibri){
+        for(int i = 0; i < nLibri; i++){
+            System.out.println(scaffale[i].toString());
+        }
     }
 }
