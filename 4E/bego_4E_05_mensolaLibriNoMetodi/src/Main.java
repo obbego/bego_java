6f00d050878e63188c92e05f3d6d78cac066da7b
obbego <150442134+obbego@users.noreply.github.com>
2024-11-05 11:19:23 +0100

    method to navigate into the shelf

--- 4E/bego_4E_05_mensolaLibriNoMetodi/src/Main.java
+++ 4E/bego_4E_05_mensolaLibriNoMetodi/src/Main.java
@@ -2,8 +2,9 @@ import static frontScreen.FrontEnd.*;
 import static utility.Tools.*;
 import java.util.ArrayList;
 import mensola.*;
-
+import java.time.LocalDate;
 import java.util.Scanner;
+import java.util.concurrent.ExecutionException;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,6 +19,8 @@ public class Main {
                 "Visualizza libri di un autore",
                 "Posizioni dei libri di un autore",
                 "Valore totale dei libri di un autore",
+                "Visita mensola",
+                "Crea mensola con valori costanti",
                 "Esci"
         };
         ArrayList <Libro> scaffale = new ArrayList<>();
@@ -91,12 +94,81 @@ public class Main {
                         System.out.println(e.getMessage());
                     }
                     break;
-
+                case 8:
+                    try {
+                        visitaMensola(keyboard, scaffale);
+                    }catch (Exception e){
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 9:
+                    nLibri = 5;
+                    assegnaLibriAutomaticamente(scaffale);
+                    break;
                 default:
                     fine = false;
                     break;
             }
         } while (fine);
+    }
+
+    public static void assegnaLibriAutomaticamente(ArrayList<Libro> mensola){
+
+        String[] listaNomi = {"Dante", "Catullo", "Leopardi", "Boccaccio", "Petrarca"};
+        String[] listaTitoli = {"Divina Commedia", "Ciao", "Canti", "Decameron", "Canzoniere"};
+
+        for(int i=0; i<5; i++) {
+            Libro libroInserito = new Libro();
+            libroInserito.numeroPagine = 5;
+            libroInserito.dataPubblicazione = LocalDate.of(2024,12,11);
+            libroInserito.tipologia = Genere.MANUALE;
+            libroInserito.titolo = listaTitoli[i];
+            libroInserito.autore = listaNomi[i];
+            mensola.add(libroInserito);
+        }
+    }
+
+    private static void visitaMensola(Scanner keyboard, ArrayList <Libro> scaffale) throws Exception {
+        String[] opzioni = {
+                "OPZIONI VISITA MENSOLA",
+                "Numero attuale",
+                "Libro precedente",
+                "Libro sucessivo",
+                "esci"
+        };
+        int posizioneAttuale = -1;
+        boolean fine = true;
+        do{
+            switch (Menu(opzioni, keyboard)){
+                case 1 -> {
+                    System.out.println("Inserisci la posizione attuale: ");
+                    posizioneAttuale = Integer.parseInt(keyboard.nextLine());
+                    if(posizioneAttuale >= scaffale.size() || posizioneAttuale < 0){
+                        throw new Exception("Il numero inserito non è valido per i valori dello scaffale");
+                    }
+                    System.out.println(scaffale.get(posizioneAttuale).toString());
+                }
+                case 2 -> {
+                    if (posizioneAttuale -1 < 0)
+                        throw new Exception("Sei arrivato al primo libro, non ci sono precedenti");
+                    else{
+                        posizioneAttuale--;
+                        System.out.println(scaffale.get(posizioneAttuale).toString());
+                    }
+                }
+                case 3 -> {
+                    if (posizioneAttuale + 1 == scaffale.size())
+                    throw new Exception("Sei arrivato all'ultimo libro, non ci sono sucessivi");
+                    else{
+                        posizioneAttuale++;
+                        System.out.println(scaffale.get(posizioneAttuale).toString());
+                    }
+                }
+                case 4 -> {
+                    fine = false;
+                }
+            }
+        }while (fine);
     }
 
 
